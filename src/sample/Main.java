be5e13e53db7904e4de15ed5278907173fb9f6cc
clementdepond <clement.depond53@gmail.com>
2020-12-02 18:12:45 +0100

    Adjust Application

--- src/sample/Main.java
+++ src/sample/Main.java
@@ -11,8 +11,8 @@ public class Main extends Application {
     @Override
     public void start(Stage primaryStage) throws Exception{
         Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
-        primaryStage.setTitle("Hello World");
-        primaryStage.setScene(new Scene(root, 632, 486));
+        primaryStage.setTitle("Ground truth tool");
+        primaryStage.setScene(new Scene(root, 662, 842));
         primaryStage.show();
     }
 
